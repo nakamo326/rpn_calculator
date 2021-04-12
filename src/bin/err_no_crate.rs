594d4c5e34bfe26b807c2847cb6ffb2aabf9dfa2
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-04-12 22:09:15 +0900

    add display trait

--- src/bin/err_no_crate.rs
+++ src/bin/err_no_crate.rs
@@ -1,11 +1,28 @@
+use std::fmt;
+
 enum MyError {
     Io(std::io::Error),
     Num(std::num::ParseIntError),
 }
 
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> {
+        match self {
+            MyError::Io(cause) => write!(f, "I/O Error: {}", cause),
+            MyError::Num(cause) => write!(f, "Parse Error: {}", cause),
+        }
+    }
+}
+
+impl From<std::io::Error> for MyError {
+    fn from(cause: std::io::Error) -> Self {
+        Self::Io(cause)
+    }
+}
+
 fn get_int_from_file() -> Result<i32, MyError> {
     let path = "number.txt";
-    let num_str = std::fs::read_to_string(path).map_err(|e| MyError::Io(e))?;
+    let num_str = std::fs::read_to_string(path).map_err(MyError::from)?;
 
     num_str
         .trim()
